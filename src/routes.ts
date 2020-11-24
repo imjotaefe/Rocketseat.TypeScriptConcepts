27ab18be8d5765db90e790f1405f06efe0713caf
João Fernando Garcia de Souza <jotaefegds@gmail.com>
2020-11-23 22:44:12 -0300

    feat: add interfaces

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express'
+import createUser from './services/createUser';
 
 export function helloWorld( request: Request, response: Response ){
-  return response.json({message: 'Hello TS'})
+  const user = createUser({name: 'joão', email: 'teste@gmail.com', password: '123', techs: [{tech: 'Java', experience: 100}]});
+  return response.json(user)
 }
